9efc6eee1166fa720ad2f29358c080db2503f948
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-11-16 00:40:28 +0330

    Tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: {
-    enabled: process.env.NODE_ENV === 'production' ? true : false,
-    content: ['./src/**/*.html', './src/**/*.tsx'],
-  },
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-};
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    purge: {
+        enabled: process.env.NODE_ENV === 'production' ? true : false,
+        content: ['./src/**/*.html', './src/**/*.tsx'],
+    },
+    theme: {
+        extend: {
+            colors: {
+                'black-blue': '#21253F',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}

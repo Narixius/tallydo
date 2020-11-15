468c70cee80c1c91739a33ca64d0aa050c1514f2
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-11-15 08:17:20 +0330

    Logo + Icon set

---
+++ src/components/TodoAdder/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../logo'
+
+function TodoAdder() {
+    return (
+        <div>
+            <Logo />
+            <p>yoo</p>
+        </div>
+    )
+}
+
+export default TodoAdder
---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+import { CheckCircleOutline } from 'heroicons-react'
+import React from 'react'
+
+function Logo() {
+    return (
+        <h1 className="font-bold text-2xl tracking-wide">
+            Tallyd
+            <CheckCircleOutline className="inline-block" />
+        </h1>
+    )
+}
+export default Logo

4ffb1e5a87dd010184ec437077534d168d8f75e3
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-11-16 10:39:11 +0330

    Todo Check Button style

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -1,6 +1,7 @@
 import { Check } from 'heroicons-react'
 import React from 'react'
 import { Todo } from '../../redux/todo/index'
+import './TodoItem.css'
 
 function todoItem({
     todo,
@@ -17,7 +18,7 @@ function todoItem({
             className="todo-item mb-2 text-black-blue p-2 bg-gray-200 rounded-md flex items-center select-none  cursor-pointer"
         >
             <div className="circle border border-black-blue rounded-full w-5 h-5">
-                {todo.isChecked() && <Check size={18} />}
+                {todo.isChecked() && <Check size={14} />}
             </div>
             <span className="ml-2">{todo.getTitle()}</span>
         </div>

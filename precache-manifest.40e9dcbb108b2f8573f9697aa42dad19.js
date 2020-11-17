3c4139140978fe03624401478552f2a11948e196
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-11-18 00:58:02 +0330

    Updates

---
+++ precache-manifest.40e9dcbb108b2f8573f9697aa42dad19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1e89174688ec691b8d8475777e8aaa",
+    "url": "/tallydo/index.html"
+  },
+  {
+    "revision": "5c27ef5dad259d3cd184",
+    "url": "/tallydo/static/css/2.abb78336.chunk.css"
+  },
+  {
+    "revision": "ca4af3ac1a90d0a5e09c",
+    "url": "/tallydo/static/css/main.bda07440.chunk.css"
+  },
+  {
+    "revision": "5c27ef5dad259d3cd184",
+    "url": "/tallydo/static/js/2.cc76ff21.chunk.js"
+  },
+  {
+    "revision": "e5051ff34247a5ecf981d18b5b70f9bc",
+    "url": "/tallydo/static/js/2.cc76ff21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083e26490c2da5338b02",
+    "url": "/tallydo/static/js/3.df9e9044.chunk.js"
+  },
+  {
+    "revision": "ca4af3ac1a90d0a5e09c",
+    "url": "/tallydo/static/js/main.948196e6.chunk.js"
+  },
+  {
+    "revision": "b201b5f8d3543761b805",
+    "url": "/tallydo/static/js/runtime-main.b593e111.js"
+  }
+]);

44a0bdc47be1d1f59ffef0c186d02c488ccc04dc
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-11-19 15:54:53 +0330

    Optimization

--- src/components/TodoAdder/Dates.tsx
+++ src/components/TodoAdder/Dates.tsx
@@ -4,18 +4,35 @@ import dayjs from 'dayjs'
 import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 import './Dates.css'
-import { DayPicker } from 'react-dates'
+import { SingleDatePicker } from 'react-dates'
+import isToday from 'dayjs/plugin/isToday'
+dayjs.extend(isToday)
 
+function generateClassName(isActive: boolean) {
+    let defaultClass =
+        'dateTag rounded-md mb-2 text-sm text-white font-bold px-3 py-1 mr-3 opacity-25 cursor-pointer'
+    if (isActive) return defaultClass + ' active'
+    else return defaultClass
+}
 function Dates({ onDateChange }: { onDateChange(date: Date): void }) {
     const [selectedDate, setSelectedDate] = React.useState('today')
     const [showCalendar, setShowCalendar] = React.useState(false)
     const [customDate, setCustomDate] = React.useState<Date>()
 
-    function generateClassName(fieldName: string) {
-        let defaultClass =
-            'dateTag rounded-md mb-2 text-sm text-white font-bold px-3 py-1 mr-3 opacity-25 cursor-pointer'
-        if (fieldName === selectedDate) return defaultClass + ' active'
-        else return defaultClass
+    const selectDate = (date: Date, state: string) => {
+        setSelectedDate(state)
+        setCustomDate(undefined)
+        onDateChange(date)
+    }
+    const customDateSelected = (e: moment.Moment | any) => {
+        setSelectedDate('customDate')
+        setCustomDate(e.toDate())
+        onDateChange(e.toDate())
+        setShowCalendar(false)
+    }
+    const selectingCustomDate = () => {
+        setSelectedDate('customDate')
+        setShowCalendar(true)
     }
     return (
         <div>
@@ -24,37 +41,35 @@ function Dates({ onDateChange }: { onDateChange(date: Date): void }) {
             </span>
             <div className="flex md:flex-wrap overflow-x-auto overflow-y-hidden hiddenScrollbar">
                 <div
-                    className={generateClassName('today') + ' bg-red-500'}
-                    onClick={() => {
-                        setSelectedDate('today')
-                        setCustomDate(undefined)
-                        onDateChange(new Date())
-                    }}
+                    className={
+                        generateClassName('today' === selectedDate) +
+                        ' bg-red-500'
+                    }
+                    onClick={() => selectDate(new Date(), 'today')}
                 >
                     <span>Today</span>
                 </div>
                 <div
-                    className={generateClassName('tomorrow') + ' bg-indigo-500'}
-                    onClick={() => {
-                        setSelectedDate('tomorrow')
-                        setCustomDate(undefined)
-                        onDateChange(dayjs().add(1, 'day').toDate())
-                    }}
+                    className={
+                        generateClassName('tomorrow' === selectedDate) +
+                        ' bg-indigo-500'
+                    }
+                    onClick={() =>
+                        selectDate(dayjs().add(1, 'day').toDate(), 'tomorrow')
+                    }
                 >
                     <span>Tomorrow</span>
                 </div>
                 <div
                     className={
-                        generateClassName('customDate') + ' bg-yellow-600 flex'
+                        generateClassName('customDate' === selectedDate) +
+                        ' bg-yellow-600 flex'
                     }
-                    onClick={() => {
-                        setSelectedDate('customDate')
-                        setShowCalendar(true)
-                    }}
+                    onClick={selectingCustomDate}
                 >
                     <CalendarOutline size={19} />
                     {customDate && (
-                        <span className="ml-1">
+                        <span className="ml-1 customDate">
                             {dayjs(customDate).format('DD-MM-YYYY')}
                         </span>
                     )}
@@ -64,14 +79,17 @@ function Dates({ onDateChange }: { onDateChange(date: Date): void }) {
             {showCalendar && (
                 <div className="relative mt-1 calendar">
                     <div className="calendar -mt-32 absolute z-50 w-full flex justify-center items-center">
-                        <DayPicker
+                        <SingleDatePicker
+                            id="signleDatePicker"
                             numberOfMonths={1}
-                            onDayClick={(e) => {
-                                setSelectedDate('customDate')
-                                setCustomDate(e.toDate())
-                                onDateChange(e.toDate())
-                                setShowCalendar(false)
-                            }}
+                            focused={true}
+                            date={null}
+                            onDateChange={customDateSelected}
+                            isDayHighlighted={(day1) =>
+                                dayjs(day1.toDate()).isToday()
+                            }
+                            onFocusChange={() => {}}
+                            withPortal
                         />
                     </div>
                 </div>

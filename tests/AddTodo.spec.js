dda9fb7c3b545cab28126f9edc22bc51087b98ce
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2021-01-14 10:44:01 +0330

    A test that fails on mobile

--- tests/AddTodo.spec.js
+++ tests/AddTodo.spec.js
@@ -1,77 +1,47 @@
 const webdriver = require('selenium-webdriver')
-// const assert = require('assert')
+const assert = require('assert')
 // const gridHost = 'hub.lambdatest.com/wd/hub'
 require('dotenv').config()
 const {By} = webdriver;
 
 describe('Add todo Lists', function () {
     browser.ignoreSynchronization = true
-    it('Add todo Lists', async function (done) {
+    it('Add todo Lists', async (done)  =>  {
         await browser.get('http://localhost:8888/')
-
         try {
-            await browser.findElement(By.css('.w-20')).click()
+            // try {
+            //     await browser.findElement(By.css('.w-20')).click()
+            // } catch (err) {
+            //     console.log('desktop')
+            // }
+
+            await browser.findElement(By.css('.px-4')).click()
+            await browser.findElement(By.css('.px-4')).sendKeys('Coffee time')
+            await browser.findElement(By.css('.bg-indigo-500 > span')).click()
+            await browser
+                .findElement(By.css('.tag:nth-child(2) > span'))
+                .click()
+            await browser.findElement(By.css('.add')).click()
+
+            await browser.sleep(1000)
+
+            // try {
+            //     await browser.findElement(By.css('.w-20')).click()
+            // } catch (err) {
+            //     console.log('desktop')
+            // }
+
+            const addedItem = await browser.findElement(
+                By.css('span.ml-2.font-medium.w-full.truncate')
+            )
+
+            assert.strictEqual(await addedItem.getText(), 'Coffee time')
+            browser.executeScript('lambda-status=passed')
+            done()
         } catch (err) {
-            console.log('desktop')
+            browser.executeScript('lambda-status=failed')
+            done.fail(err)
         }
-
-        await browser.findElement(By.css('.px-4')).click()
-        await browser.findElement(By.css('.px-4')).sendKeys('Coffee time')
-        await browser.findElement(By.css('.bg-indigo-500 > span')).click()
-        await browser.findElement(By.css('.tag:nth-child(2) > span')).click()
-        await browser.findElement(By.css('.add')).click()
-
-        await driver.sleep(1000)
-
-        try {
-            await browser.findElement(By.css('.w-20')).click()
-        } catch (err) {
-            console.log('desktop')
-        }
-
-        const addedItem = await browser.findElement(
-            By.css('span.ml-2.font-medium.w-full.truncate')
-        )
-
-        expect(await addedItem.getText(), 'Coffee time')
-
-        browser.executeScript('lambda-status=passed')
-
-        done()
-    })
-
-    it('Fail Add Todo', async function (done) {
-        await browser.get('http://localhost:8888/')
-
-        try {
-            await browser.findElement(By.css('.w-20')).click()
-        } catch (err) {
-            console.log('desktop')
-        }
-
-        await browser.findElement(By.css('.px-4')).click()
-        await browser.findElement(By.css('.px-4')).sendKeys('Coffee time')
-        await browser.findElement(By.css('.bg-indigo-500 > span')).click()
-        await browser.findElement(By.css('.tag:nth-child(2) > span')).click()
-        await browser.findElement(By.css('.add')).click()
-
-        await driver.sleep(1000)
-
-        try {
-            await browser.findElement(By.css('.w-20')).click()
-        } catch (err) {
-            console.log('desktop')
-        }
-
-        const addedItem = await browser.findElement(
-            By.css('span.ml-2.font-medium.w-full.truncate')
-        )
-
-        expect(await addedItem.getText(), 'Time to Cofee')
-
-        browser.executeScript('lambda-status=passed')
-
-        done()
     })
 
 })
